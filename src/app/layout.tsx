19a492f5c6d5c716d4738d07a32480abd3a5a15d
aurliaas888-debug <aurlia.as888@gmail.com>
2025-12-15 22:13:50 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,6 +42,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta name="facebook-domain-verification" content="31ss2bobsi284siz0z80s7tze8yg9s" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
       >
